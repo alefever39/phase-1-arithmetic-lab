db63d69461069c04f65f81a6056e92db35782def
Allie LeFever <alefever39@gmail.com>
2022-05-16 17:20:53 -0600

    commit tree 5c1b8bc1d10a451238a2abf5df2636a2d92b495b

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = parseInt((Math.random() * 10) + 1);
+console.log(random);
+
+const num3 = 4;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 9, 12, 20, 19);
